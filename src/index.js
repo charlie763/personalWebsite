3b36209fd881a2fbf589a320475205bf6bfaf69c
charlie763 <cwisoff@gmail.com>
2019-06-17 08:33:19 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,262 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import headshot from './headshot.png';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+// import 'bootstrap/dist/js/bootstrap.min.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+document.body.classList.add('bg-water');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component{
+
+	render(){
+		return(
+			<div className="page">
+				<Page />
+			</div>
+		);
+	}
+
+}
+
+class Page extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			activeBody: 'Home',
+			activeNav: 'Professional Life',
+		};
+	}
+
+	changeBody = (body) => {
+		this.setState({activeBody: body})
+	};
+
+	changeNav = (nav) => {
+		this.setState({activeNav: nav})
+	};
+
+	render(){
+		return(
+			<div className="bg-water">
+				<NavBar handleBodyChange={this.changeBody} 
+						handleNavChange={this.changeNav}
+						activeNav={this.state.activeNav}
+				/>
+				<Body activeBody={this.state.activeBody} />
+			</div>
+		);
+	}
+}
+
+class NavBar extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			menuOpen: false,
+		};
+	}
+
+	primaryNavButton(navTo){
+		return(
+			<div className="col-6 justify-content-start">
+				<button className="btn bg-plaster text-white" 
+						onClick={()=> this.props.handleNavChange(navTo)}>{navTo}
+				</button>
+			</div>
+		);
+	}
+
+	secondaryNavButton(navTo){
+		return(
+			<button 
+				className="btn bg-water text-linen"
+				onClick={()=> this.props.handleBodyChange(navTo)}>{navTo}
+			</button>
+			);
+	}
+
+	menuNavButton(navTo){
+		return(
+			<button 
+				className="btn bg-linen text-ice"
+				onClick={()=> this.props.handleBodyChange(navTo)}>{navTo}
+			</button>
+			);
+	}
+
+	menuIcon(){
+		return(
+			<div onClick={()=> this.setState({menuOpen: !this.state.menuOpen})} style={{ "cursor": "pointer"}}>
+				<div className="bg-linen my-2" style={{"height": ".25em", "width": "2em"}}></div>
+				<div className="bg-linen my-2" style={{"height": ".25em", "width": "2em"}}></div>
+				<div className="bg-linen my-2" style={{"height": ".25em", "width": "2em"}}></div>
+			</div>
+			);
+	}
+
+	openMenu(){
+		if (this.state.menuOpen === true){
+			return(
+				<div className="row mt-5 bg-linen rounded" style={{"z-index": "2", "position": "absolute", "right": "2em"}}>
+					<div className="col">
+						<div className="row">
+							{this.menuNavButton('Home')}
+						</div>
+						<div className="row">
+							{this.menuNavButton('My Story')}
+						</div>
+						<div className="row">
+							{this.menuNavButton('Resume')}
+						</div>
+					</div>
+				</div>
+				);
+		} 
+	}
+
+	render(){
+		let activeNav = this.props.activeNav
+		if(activeNav === 'Professional Life'){
+			return(
+				<div className="container my-4">
+					<div className="row">
+						{this.primaryNavButton('Personal Life')}
+						<div className="col-6 d-none d-lg-flex justify-content-end">
+							{this.secondaryNavButton('Home')}
+							{this.secondaryNavButton('My Story')}
+							{this.secondaryNavButton('Resume')}
+							{this.secondaryNavButton('Blog')}
+						</div>
+						<div class="col-6 d-flex d-lg-none justify-content-end">
+							{this.menuIcon()}
+							{this.openMenu()}
+						</div>
+					</div>
+				</div>
+			);
+		} else if(activeNav === 'Personal Life'){
+			return(
+				<div className="container my-4">
+					<div className="row">
+						{this.primaryNavButton('Professional Life')}
+						{this.secondaryNavButton('Home')}
+						{this.secondaryNavButton('Family')}
+						{this.secondaryNavButton('Music')}
+						{this.secondaryNavButton('Origami')}
+					</div>
+				</div>
+			);	
+		}
+	}
+}
+
+function paragraph(text){
+		return(
+			<p className="text-white my-2">{text}</p>
+			);
+	}
+
+class Home extends React.Component{
+	render(){
+		return(
+			<div className="container mt-3">
+				<div className="row justify-content-center mt-5">
+					<h1 className="text-white">Hi, I'm Charlie</h1>
+				</div>
+				<div className="row justify-content-center mb-sm-5">
+					<h5 className="subtitle text-white">Product Manager + Radical Empathy</h5>
+				</div>
+				<div className="row justify-content-center py-4">
+					<div className="col-sm-4 d-flex justify-content-center">
+						<div className="image-wrapper">
+							<img className="w-100" src={headshot}></img>
+						</div>
+					</div>
+					<div className="col-sm-6 m-4 pl-5 justify-content-center d-flex flex-column">
+						{paragraph('I love to understand what makes people tick and build products that solve real needs.')}
+						{paragraph('Once upon a time, I founded and ran my own startup.')}
+						{paragraph('Alum of Brown University living in Albuquerque, NM	')} 	 					
+					</div>
+				</div>
+			</div>
+			);
+		}
+}
+
+class MyStory extends React.Component{
+	render(){
+		return(
+			<div className="container mt-3">
+				<div className="row justify-content-center mt-5">
+					<h1 className="text-white">My Story</h1>
+				</div>
+				<div className="row d-flex justify-content-center">
+					<div className="col-8 m-4 pl-5 justify-content-center d-flex flex-column">
+						{paragraph('I never intended to become a tech entrepreneur. I never set out to be a product manager. It started because I thought I had a great idea. At the time, I was working for the Kettering Foundation. I was passionate about empowering individuals to better influence democracy and our political system. I had been studying the intersection between social network analysis and democracy. I was fascinated by studies that showed people with the highest' + html(<a href="https://en.wikipedia.org/wiki/Betweenness_centrality">betweenness centrality</a>) + 'in their social networks, for example the Medici’s in Renaissance Italy, often had more power or social capital. My great idea was to help the average individual understand how they might better navigate their civic and political networks. I wanted the average individual to be able to have the power of a Medici, to have the power of a lobbyists who often spend their career learning to map and navigate this civic and political networks.')}
+						{paragraph('Once upon a time, I founded and ran my own startup.')}
+						{paragraph('Alum of Brown University living in Albuquerque, NM	')} 	 					
+					</div>
+				</div>
+			</div>
+			);
+		}
+}
+
+class Resume extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			lens: 'hidden',
+			civnet: 'hidden',
+			kettering: 'hidden',
+		};
+	}
+
+	bullet(text){}
+
+	header(text){
+
+	}
+
+	titleLine(company, title, timeline){}
+	subitle(text){}
+
+	render(){
+		return(
+			<div></div>
+			);
+	}
+}
+
+class Body extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		switch(this.props.activeBody) {
+			case 'Home':
+				return(
+					<div>
+						<Home />
+					</div>
+				);
+			case 'My Story':
+				return(
+					<div>
+						<MyStory />
+					</div>
+				);
+		}
+	}
+}
+
+
+
+ReactDOM.render(
+        <App />, 
+        document.getElementById('root')
+      );
