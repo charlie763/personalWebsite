7f5e8ca34b8177e38f8c560eda0229ee2daeefd9
charlie763 <cwisoff@gmail.com>
2020-09-14 14:22:04 -0600

    work on badges

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -19,6 +19,11 @@ function renderAppLink(app_url){
   }
 }
 
+// function renderTag(tag){
+//   return(<span class="badge badge-pill">{tag}</span>);
+// }
+
+
 function repo_block(repo_hash){
   return(
     <div className="row justify-content-center my-3">
@@ -64,7 +69,8 @@ class Portfolio extends React.Component{
       blog_url: "https://medium.com/@cwisoff/trying-test-driven-development-for-the-first-time-with-rspec-and-capybara-1cde61789f2a",
       github_url: "https://github.com/charlie763/tetris",
       screenshot: "https://drive.google.com/uc?id=1_Atu_XIBtnqKQiWYQsWo2I4CjmFMG5ip",
-      app_url: "https://charlie763.github.io/tetris/frontend/"
+      app_url: "https://charlie763.github.io/tetris/frontend/",
+      tags: ["javascript", "rails"]
       })
     }
 
@@ -74,7 +80,8 @@ class Portfolio extends React.Component{
       blog_url: "https://medium.com/@cwisoff/trying-test-driven-development-for-the-first-time-with-rspec-and-capybara-1cde61789f2a",
       github_url: "https://github.com/charlie763/code-notes",
       screenshot: "https://i.postimg.cc/cCBRMpqg/screenshot-homepage.png",
-      app_url: "notes-on-code.herokuapp.com/"
+      app_url: "notes-on-code.herokuapp.com/",
+      tags: ["javascript", "rails"]
       })
     }
     
@@ -84,7 +91,8 @@ class Portfolio extends React.Component{
       blog_url: "https://medium.com/@cwisoff/sinatra-portfolio-project-the-art-of-debugging-eba911b05e16",
       github_url: "https://github.com/charlie763/github-portfolio-cms",
       screenshot: github_screenshot,
-      app_url: "https://infinite-cove-25560.herokuapp.com/"
+      app_url: "https://infinite-cove-25560.herokuapp.com/",
+      tags: ["javascript", "rails"]
       })
     }
 
@@ -93,21 +101,24 @@ class Portfolio extends React.Component{
       description: "This was my first Flatiron Portfolio Project. It demonstrates my competency in Ruby, Object-Oriented Programming, APIs and web scraping. The app allows users to search news articles by topic and return snippets from returned articles based on additional keyword search. The intention is make it easy for someone to find relevant news related to a topic of interest. It integrates with the Guardian API, scrapes from Guardian article pages, and is meant as a proof-of-concept to be extended to additional news APIs.",
       blog_url: "https://medium.com/@cwisoff/news-search-cli-data-gem-portfolio-project-3c41683cd635",
       github_url: "https://github.com/charlie763/news_search_cli",
-      screenshot: cli_screenshot
+      screenshot: cli_screenshot,
+      tags: ["javascript", "rails"]
       })
     }
 
     {repo_block({title: "Personal Website",
       created: "May, 2019",
       description: "If you're on my portfolio, this is the code for the personal website you're visiting. I began building this website in between product management jobs. At the time, I used it as an opportunity to learn React and brush up on my Bootstrap skills. For example, the navbar uses React's notion of state to maintain the active nav state.",
-      github_url: "https://github.com/charlie763/personalWebsite"
+      github_url: "https://github.com/charlie763/personalWebsite",
+      tags: ["javascript", "rails"]
       })
     }
 
     {repo_block({title: "Checkers",
       created: "March, 2019",
       description: "In between jobs, I took the opportunity to teach myself Ruby and get better at object-oriented programming. Years before, I had taught myself to code in Python. I thought it would be useful to learn a different language. The application implements a basic 2-player checkers games using the Ruby 2D gem for visualization.",
-      github_url: "https://github.com/charlie763/checkers"
+      github_url: "https://github.com/charlie763/checkers",
+      tags: ["javascript", "rails"]
       })
     }
   
